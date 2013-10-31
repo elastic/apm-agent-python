d43fe4a84a78fd17c11b74939ef8f211cb8d6cad
Kristian Øllegaard <kristian@kristian.io>
2013-10-31 15:21:00 +0100

    python-rq contrib module

---
+++ opbeat/contrib/rq/__init__.py
@@ -0,0 +1,27 @@
+def register_opbeat(client, worker):
+    """Given a Opbeat client and an RQ worker, registers exception handlers
+    with the worker so exceptions are logged to Opbeat.
+
+    E.g.:
+
+    from opbeat.contrib.django.models import client
+    from opbeat.contrib.rq import register_opbeat
+    
+    worker = Worker(map(Queue, listen))
+    register_opbeat(client, worker)
+    worker.work()
+
+    """
+    def send_to_sentry(job, *exc_info):
+        client.captureException(
+            exc_info=exc_info,
+            extra={
+                'job_id': job.id,
+                'func': job.func_name,
+                'args': job.args,
+                'kwargs': job.kwargs,
+                'description': job.description,
+            }
+        )
+
+    worker.push_exc_handler(send_to_sentry)
