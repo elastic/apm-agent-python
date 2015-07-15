c52f6679f90a7faa8ef1dd7c0dbb941d541386ac
Ron Cohen <ron@opbeat.com>
2015-07-15 22:50:09 +0200

    'Kind' of django transactions are now transaction.django.

--- tests/instrumentation/django_tests/template_tests.py
+++ tests/instrumentation/django_tests/template_tests.py
@@ -92,7 +92,7 @@ class TracesTest(TestCase):
         self.assertEqual(len(transactions), 1)
         self.assertEqual(len(traces), 2, [t['signature'] for t in traces])
 
-        kinds = ['transaction', 'template.jinja2']
+        kinds = ['transaction.django', 'template.jinja2']
         self.assertEqual(set([t['kind'] for t in traces]),
                          set(kinds))
 
@@ -100,7 +100,7 @@ class TracesTest(TestCase):
         kinds_dict = dict([(t['kind'], t) for t in traces])
         traces = [kinds_dict[k] for k in kinds]
 
-        self.assertEqual(traces[0]['kind'], 'transaction.django.view')
+        self.assertEqual(traces[0]['kind'], 'transaction.django')
         self.assertEqual(traces[0]['signature'], 'transaction')
         self.assertEqual(traces[0]['transaction'],
                          'tests.contrib.django.testapp.views.render_jinja2_template')
