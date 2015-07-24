7a416b6681eb2d95b5a2aed2b2ebe4224530f2b6
Benjamin Wohlwend <benjamin@opbeat.com>
2015-07-24 12:06:33 +0200

    use pytest.importskip to import gevent/zerorpc

--- tests/contrib/zerorpc/zeropc_tests.py
+++ tests/contrib/zerorpc/zeropc_tests.py
@@ -4,20 +4,15 @@ import shutil
 import tempfile
 import pytest
 
-try:
-    import zerorpc  # no python 3 support for zerorpc
-    has_zerorpc = True
-except (ImportError, OSError):
-    # pypy throws OSError
-    has_zerorpc = False
+zerorpc = pytest.importorskip("zerorpc")
+gevent = pytest.importorskip("gevent")
 
 from opbeat.contrib.zerorpc import OpbeatMiddleware
 
 from django.test import TestCase
 from tests.helpers import get_tempstoreclient
 
-@pytest.mark.skipif(not has_zerorpc,
-                    reason="no zerorpc/gevent available (python3/pypy)")
+
 class ZeroRPCTest(TestCase):
     def setUp(self):
         self._socket_dir = tempfile.mkdtemp(prefix='opbeatzerorpcunittest')
@@ -31,8 +26,6 @@ class ZeroRPCTest(TestCase):
         ))
 
     def test_zerorpc_middleware_with_reqrep(self):
-        import gevent
-
         self._server = zerorpc.Server(random)
         self._server.bind(self._server_endpoint)
         gevent.spawn(self._server.run)
@@ -51,7 +44,6 @@ class ZeroRPCTest(TestCase):
             self.assertEqual(frames[0]['function'], 'choice')
             self.assertEqual(frames[0]['module'], 'random')
             return
-
         self.fail('An IndexError exception should have been raised an catched')
 
     def tearDown(self):
