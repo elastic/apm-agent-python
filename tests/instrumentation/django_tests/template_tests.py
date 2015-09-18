07c7bd92ae259ea27977db1329793a6e772227df
Ron Cohen <ron@opbeat.com>
2015-09-18 13:33:47 +0200

    Transaction traces should have 'transaction' kind.

--- tests/instrumentation/django_tests/template_tests.py
+++ tests/instrumentation/django_tests/template_tests.py
@@ -100,7 +100,7 @@ class TracesTest(TestCase):
         kinds_dict = dict([(t['kind'], t) for t in traces])
         traces = [kinds_dict[k] for k in kinds]
 
-        self.assertEqual(traces[0]['kind'], 'transaction.django')
+        self.assertEqual(traces[0]['kind'], 'transaction')
         self.assertEqual(traces[0]['signature'], 'transaction')
         self.assertEqual(traces[0]['transaction'],
                          'tests.contrib.django.testapp.views.render_jinja2_template')
