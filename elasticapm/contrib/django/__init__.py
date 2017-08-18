4af7a2db6c036cb0b1655058d7a160524fa4a36a
Benjamin Wohlwend <piquadrat@gmail.com>
2017-08-18 14:54:35 +0200

    fix remaining EXXX flake8 errors, except for E731, which we'll keep
    E731 is "do not assign a lambda expression, use a def", which we
    use a lot in the proxy client

--- elasticapm/contrib/django/__init__.py
+++ elasticapm/contrib/django/__init__.py
@@ -8,7 +8,6 @@ Large portions are
 :copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+from elasticapm.contrib.django.client import *
 
 default_app_config = 'elasticapm.contrib.django.apps.ElasticAPMConfig'
-
-from elasticapm.contrib.django.client import *
