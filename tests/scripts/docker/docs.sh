a95f9752aa7b89d35f0c1d2f58cc65dfce1a3e3c
Silvia Mitter <simitt@users.noreply.github.com>
2017-09-07 15:07:35 +0200

    Stop docker containers and fix redis port. (#30)
    * Cleanup after job, bugfixes.
    
    * Stop & clean docker containers before running the tests.
    * Fix redis port to use default 6379.
    * Adapt Jenkinsfile scripts to use workspace directory.
    * Avoid writing pyc files
    * Add Dockerfile and only copy workspace.
    
    * Fail fast when script fails. Move cleanup to file.
    
    * Immediately fail job when something in script fails.
    * Move cleanup to file, do not fail when no docker container is running or found.

--- tests/scripts/docker/docs.sh
+++ tests/scripts/docker/docs.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+set -e
+
 docker build -t "lint-docs" -f ./DockerfileDocs .
 docker run -i "lint-docs"
--- tests/scripts/docker/run_tests.sh
+++ tests/scripts/docker/run_tests.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
+set -e
+
 if [ $# -lt 2 ]; then
   echo "Arguments missing"
   exit 2
 fi
 
-pip_cache=${3-"$HOME/.cache/pip"}
+pip_cache="$HOME/.cache/pip"
 docker_pip_cache="/app/.cache/pip"
-mkdir -p ${pip_cache}
-
-export PYTHON_IMAGE=$1
 
 PYTHON_TYPE="python"
 if [[ $1 == *"pypy"*  ]]; then
   PYTHON_TYPE="pypy"
 fi
 
-docker-compose pull run_tests
+docker-compose build --build-arg PYTHON_IMAGE=$1 run_tests
 docker-compose run \
-  -e WEBFRAMEWORK=$2 -e PIP_CACHE=${docker_pip_cache} -e PYTHON_TYPE=$PYTHON_TYPE \
-  -v `pwd`:/app -v ${pip_cache}:${docker_pip_cache} \
-  --rm run_tests
+  -e PYTHONDONTWRITEBYTECODE=1 -e WEBFRAMEWORK=$2 -e PIP_CACHE=${docker_pip_cache} -e PYTHON_TYPE=$PYTHON_TYPE \
+  -v ${pip_cache}:${docker_pip_cache} \
+  --rm run_tests \
+	/bin/bash \
+  -c "./tests/scripts/run_tests.sh"
