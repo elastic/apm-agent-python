4b9c72dde373537c19fa6c3048bccdede43a1b9e
Benjamin Wohlwend <beni@elastic.co>
2017-11-03 14:06:40 +0100

    get rid of a whole lot of complexity in test requirements by using environment markers in requirements file

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -2,20 +2,7 @@
 
 set -ex
 
-PYTHON_MAJOR_VERSION=$(python -c "import sys; print(sys.version_info[0])");
-pip install -U pip
+pip install -U pip --cache-dir "${PIP_CACHE}"
 pip install -r "tests/requirements/requirements-${WEBFRAMEWORK}.txt" --cache-dir "${PIP_CACHE}"
-pip install -r "tests/requirements/requirements-python-${PYTHON_MAJOR_VERSION}.txt" --cache-dir "${PIP_CACHE}"
-if [[ $PYTHON_VERSION == 3.5* ]]; then
-  pip install -r tests/requirements/requirements-asyncio.txt --cache-dir "${PIP_CACHE}"
-fi
-if [[ $PYTHON_TYPE == 'pypy' ]]; then
-  pip install -r tests/requirements/requirements-pypy.txt --cache-dir "${PIP_CACHE}"
-else
-  pip install -r tests/requirements/requirements-cpython.txt --cache-dir "${PIP_CACHE}"
-  if [[ $PYTHON_MAJOR_VERSION == '2' ]]; then
-    pip install -r tests/requirements/requirements-zerorpc.txt --cache-dir "${PIP_CACHE}"
-  fi
-fi
 
 make test
