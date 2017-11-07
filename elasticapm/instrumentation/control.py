e8410dad38ac1c74111ffb10adb4b3b71dd3034f
Benjamin Wohlwend <beni@elastic.co>
2017-11-07 11:38:21 +0100

    added `uninstrument` function to reverse instrumentation (#90)
    closes #90

--- elasticapm/instrumentation/control.py
+++ elasticapm/instrumentation/control.py
@@ -1,6 +1,23 @@
+import threading
+
 from elasticapm.instrumentation import register
+
+_lock = threading.Lock()
 
 
 def instrument():
-    for obj in register.get_instrumentation_objects():
-        obj.instrument()
+    """
+    Instruments all registered methods/functions with a wrapper
+    """
+    with _lock:
+        for obj in register.get_instrumentation_objects():
+            obj.instrument()
+
+
+def uninstrument():
+    """
+    If present, removes instrumentation and replaces it with the original method/function
+    """
+    with _lock:
+        for obj in register.get_instrumentation_objects():
+            obj.uninstrument()
