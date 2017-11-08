09a767c22855470c733040b140c3990fdb85338e
Benjamin Wohlwend <beni@elastic.co>
2017-11-08 17:02:22 +0100

    move docker stuff used for Jenkins testing into tests folder [WIP]
    This necessitates to mount the project folder as a volume instead
    of `ADD`ing it at build time. The benefit of this is that we don't
    need to build new docker images on every run, the drawback is that
    we need to jump through some hoops to ensure that files created
    during the tests use the same user as Jenkins.

--- tests/scripts/docker/docs.sh
+++ tests/scripts/docker/docs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+cd tests
+
 docker build -t "lint-docs" -f ./DockerfileDocs .
-docker run -i "lint-docs"
+docker run -e LOCAL_USER_ID=$UID \
+           -v "$(dirname $(pwd))":/app \
+           -i "lint-docs" \
+           make docs
