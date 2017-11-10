39cbf2f37ca413e0b83bae7e7ae4bf5363d92daa
Benjamin Wohlwend <beni@elastic.co>
2017-11-10 13:09:06 +0100

    move docker stuff used for Jenkins testing into tests folder (#94)
    This necessitates to mount the project folder as a volume instead
    of `ADD`ing it at build time. The benefit of this is that we don't
    need to build new docker images on every run, the drawback is that
    we need to jump through some hoops to ensure that files created
    during the tests use the same user as Jenkins.
    
    closes #94

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-pip install -U pip --cache-dir "${PIP_CACHE}"
-pip install -r "tests/requirements/requirements-${WEBFRAMEWORK}.txt" --cache-dir "${PIP_CACHE}"
+pip install --user -U pip --cache-dir "${PIP_CACHE}"
+pip install --user -r "tests/requirements/requirements-${WEBFRAMEWORK}.txt" --cache-dir "${PIP_CACHE}"
+
+export PATH=/home/user/.local/bin:$PATH
 
 make test
