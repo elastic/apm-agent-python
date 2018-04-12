523d384a0f43ffb15e957e7486500c164522fa66
Benjamin Wohlwend <beni@elastic.co>
2018-04-12 16:57:34 +0200

    wait for elasticsearch to boot up

--- tests/scripts/envs/elasticsearch-6.sh
+++ tests/scripts/envs/elasticsearch-6.sh
@@ -1,3 +1,5 @@
 export PYTEST_MARKER="-m elasticsearch"
-export ES_URL=$ES_6_URL
+export ES_URL="http://elasticsearch6:9200"
 export DOCKER_DEPS="elasticsearch6"
+export WAIT_FOR_HOST="elasticsearch6"
+export WAIT_FOR_PORT=9200
