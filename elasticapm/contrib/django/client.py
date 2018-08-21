6fb14765e0113d6a943f48790734789b7e3b573c
Benjamin Wohlwend <beni@elastic.co>
2018-08-21 11:33:02 +0200

    iterate over Django response headers correctly instead of using private API (#265)
    closes #265
    
    Signed-off-by: Benjamin Wohlwend <beni@elastic.co>

--- elasticapm/contrib/django/client.py
+++ elasticapm/contrib/django/client.py
@@ -140,11 +140,8 @@ class DjangoClient(Client):
     def get_data_from_response(self, response):
         result = {"status_code": response.status_code}
 
-        # Django does not expose a public API to iterate over the headers of a response.
-        # Unfortunately, we have to access the private _headers dictionary here, which is
-        # a mapping of the form lower-case-header: (Original-Header, value)
-        if getattr(response, "_headers", {}):
-            result["headers"] = {key: value[1] for key, value in response._headers.items()}
+        if hasattr(response, "items"):
+            result["headers"] = dict(response.items())
         return result
 
     def capture(self, event_type, request=None, **kwargs):
