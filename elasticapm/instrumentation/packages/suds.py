87f3ac9b4882634ec99be5b2a97bce833ec4a9d3
Ismael <ismaelgfk@gmail.com>
2018-11-07 16:20:14 -0300

    Add instrumentation to SUDS as it downloads and parses the WSDL

--- elasticapm/instrumentation/packages/suds.py
+++ elasticapm/instrumentation/packages/suds.py
@@ -5,10 +5,14 @@ from elasticapm.traces import capture_span
 class SUDSInstrumentation(AbstractInstrumentedModule):
     name = "suds"
 
-    instrument_list = [("suds.client", "SoapClient.invoke")]
+    instrument_list = [("suds.client", "SoapClient.invoke"), ("suds.client", "Client.__init__")]
 
     def call(self, module, method, wrapped, instance, args, kwargs):
-        signature = 'suds.service.' + instance.method.name
+        signature = ''
+        if method == 'Client.__init__':
+            signature = 'Parse WSDL ' + args[0]
+        elif method == 'SoapClient.invoke':
+            signature = instance.method.name
+
         with capture_span(signature, "suds"):
             return wrapped(*args, **kwargs)
-
