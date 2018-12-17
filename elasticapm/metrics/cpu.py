6ed4a4bf9cd2b92a6afe557b617be1894df1c261
Benjamin Wohlwend <beni@elastic.co>
2018-12-17 15:17:14 +0100

    moved metric sets into sub package, and added some docs

--- elasticapm/metrics/cpu.py
+++
@@ -1,7 +0,0 @@
-import os
-import platform
-
-if platform.system() == "Linux" and "ELASTIC_APM_FORCE_PSUTIL_METRICS" not in os.environ:
-    from elasticapm.metrics.cpu_linux import CPUMetricSet  # noqa: F401
-else:
-    from elasticapm.metrics.cpu_psutil import CPUMetricSet  # noqa: F401
--- elasticapm/metrics/sets/cpu_psutil.py
+++ elasticapm/metrics/sets/cpu_psutil.py
@@ -12,15 +12,14 @@ class CPUMetricSet(MetricsSet):
         super(CPUMetricSet, self).__init__()
 
     def collect(self):
-        cpu_count = float(psutil.cpu_count())
-        self.gauge("system.cpu.total.norm.pct").val = psutil.cpu_percent(interval=None) / cpu_count
+        self.gauge("system.cpu.total.norm.pct").val = psutil.cpu_percent(interval=None) / 100.0
         self.gauge("system.memory.actual.free").val = psutil.virtual_memory().available
         self.gauge("system.memory.total").val = psutil.virtual_memory().total
 
         p = psutil.Process()
         with p.oneshot():
             memory_info = p.memory_full_info()
-            self.gauge("system.process.cpu.total.norm.pct").val = p.cpu_percent() / cpu_count
+            self.gauge("system.process.cpu.total.norm.pct").val = p.cpu_percent() / 100.0
             self.gauge("system.process.memory.size").val = memory_info.vms
             self.gauge("system.process.memory.rss.bytes").val = memory_info.rss
         return super(CPUMetricSet, self).collect()
