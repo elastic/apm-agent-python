7ab887d131fcdd43941eb02079f8739a8d0d8a00
Benjamin Wohlwend <beni@elastic.co>
2018-12-18 14:46:04 +0100

    fixed psutil calculations and added a test and test matrix

--- tests/scripts/docker/run_tests.sh
+++ tests/scripts/docker/run_tests.sh
@@ -55,4 +55,4 @@ PYTHON_VERSION=${1} docker-compose run \
   -v "$(dirname $(pwd))":/app \
   --rm run_tests \
 	/bin/bash \
-  -c "timeout 5m ./tests/scripts/run_tests.sh"
+  -c "timeout 5m /bin/bash ./tests/scripts/run_tests.sh"
