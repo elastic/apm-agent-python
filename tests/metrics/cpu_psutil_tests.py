aca343971fa6bf2fd1f5585f0fd306bf77782538
Benjamin Wohlwend <beni@elastic.co>
2018-12-19 10:23:45 +0100

    added test to compare output from psutil with our own proc parsing

--- tests/metrics/cpu_psutil_tests.py
+++ tests/metrics/cpu_psutil_tests.py
@@ -25,3 +25,31 @@ def test_cpu_mem_from_psutil():
 
     assert data["samples"]["system.process.memory.rss.bytes"]["value"] > 0
     assert data["samples"]["system.process.memory.size"]["value"] > 0
+
+
+cpu_linux = pytest.importorskip("elasticapm.metrics.sets.cpu_linux")
+
+
+def test_compare_psutil_linux_metricsets():
+    psutil_metricset = cpu_psutil.CPUMetricSet()
+    linux_metricset = cpu_linux.CPUMetricSet()
+    # do something that generates some CPU load
+    for i in compat.irange(10 ** 6):
+        j = i * i
+    psutil_data = psutil_metricset.collect()
+    linux_data = linux_metricset.collect()
+
+    assert (
+        abs(
+            psutil_data["samples"]["system.cpu.total.norm.pct"]["value"]
+            - linux_data["samples"]["system.cpu.total.norm.pct"]["value"]
+        )
+        < 0.02
+    )
+    assert (
+        abs(
+            psutil_data["samples"]["system.process.cpu.total.norm.pct"]["value"]
+            - linux_data["samples"]["system.process.cpu.total.norm.pct"]["value"]
+        )
+        < 0.02
+    )
