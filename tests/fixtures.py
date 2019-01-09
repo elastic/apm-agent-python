9de1b979c3ac23efbcc42a5c1321a8b688a22992
Gil Raphaelli <g@raphaelli.com>
2019-01-09 03:13:11 -0500

    update paths to json schemas (#381)

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -27,9 +27,9 @@ except ImportError:
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 
-ERRORS_SCHEMA = os.path.join(cur_dir, ".schemacache", "errors", "v2_error.json")
-TRANSACTIONS_SCHEMA = os.path.join(cur_dir, ".schemacache", "transactions", "v2_transaction.json")
-SPAN_SCHEMA = os.path.join(cur_dir, ".schemacache", "spans", "v2_span.json")
+ERRORS_SCHEMA = os.path.join(cur_dir, ".schemacache", "errors", "error.json")
+TRANSACTIONS_SCHEMA = os.path.join(cur_dir, ".schemacache", "transactions", "transaction.json")
+SPAN_SCHEMA = os.path.join(cur_dir, ".schemacache", "spans", "span.json")
 METADATA_SCHEMA = os.path.join(cur_dir, ".schemacache", "metadata.json")
 
 assert os.path.exists(ERRORS_SCHEMA) and os.path.exists(
