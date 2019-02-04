0099b669aecea47b2fb0514a1a422e86a0901d79
Doug Byrne <doug@shareablee.com>
2019-02-04 09:33:56 -0500

    Handle datetime objects in elasticsearch queries

--- elasticapm/instrumentation/packages/elasticsearch.py
+++ elasticapm/instrumentation/packages/elasticsearch.py
@@ -44,7 +44,7 @@ class ElasticsearchConnectionInstrumentation(AbstractInstrumentedModule):
                 # we assume utf8, which is the default
                 query.append("q=" + params["q"].decode("utf-8", errors="replace"))
             if isinstance(body, dict) and "query" in body:
-                query.append(json.dumps(body["query"]))
+                query.append(json.dumps(body["query"], default=str))
             context["db"]["statement"] = "\n\n".join(query)
         elif api_method == "Elasticsearch.update":
             if isinstance(body, dict) and "script" in body:
