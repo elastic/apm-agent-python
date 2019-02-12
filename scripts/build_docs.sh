3e7a3c68dc09858a482dc57d7f859b3ceb6e2cdd
Benjamin Wohlwend <beni@elastic.co>
2019-02-12 14:55:00 +0100

    update docs build command for new setup

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -26,6 +26,6 @@ dest_dir="$html_dir/${name}"
 mkdir -p "$dest_dir"
 params="--chunk=1"
 if [ "$PREVIEW" = "1" ]; then
-  params="--chunk=1 -open chunk=1 -open"
+  params="--chunk=1 --open"
 fi
-$docs_dir/build_docs.pl $params --doc "$index" -out "$dest_dir"
+$docs_dir/build_docs $params --doc "$index" --out "$dest_dir"
