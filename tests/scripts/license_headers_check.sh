4f48b3447ad48ed79c246c03a9d083ea8850b73a
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-06-11 15:52:10 +0100

    Exclude check_paths_for_matches.py from the license header validation

--- tests/scripts/license_headers_check.sh
+++ tests/scripts/license_headers_check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ $# -eq 0 ]]
 then
-    FILES=$(find . -iname "*.py" -not -path "./elasticapm/utils/wrapt/*" -not -path "./dist/*" -not -path "./build/*" -not -path "./tests/utils/stacks/linenos.py")
+    FILES=$(find . -iname "*.py" -not -path "./elasticapm/utils/wrapt/*" -not -path "./dist/*" -not -path "./build/*" -not -path "./tests/utils/stacks/linenos.py" -not -path "./.ci/bin/check_paths_for_matches.py")
 else
     FILES=$@
 fi
