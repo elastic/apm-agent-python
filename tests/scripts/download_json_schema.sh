bb34b27c964e2efcda764f48327feaffb92f6f83
Colton Myers <colton.myers@gmail.com>
2019-09-04 11:38:43 -0600

    Test setup and documentation fixes (#584)
    * Fix `make json schema` for macOS
    
    Use gtar instead of tar
    
    * Add psutil to requirement-base
    
    Had a unit test failure without it installed:
    
    test_send_remote_failover_sync_non_transport_exception_error
    
    * Add some testing artifacts to .gitignore
    
    * Fix a couple of testing documentation issues
    
    * Fix psutil test properly
    
    test_send_remote_failover_sync_non_transport_exception_error isn't
    concerned with metrics, so just disable the metrics sets.

--- tests/scripts/download_json_schema.sh
+++ tests/scripts/download_json_schema.sh
@@ -7,7 +7,11 @@ download_schema()
     rm -rf ${1} && mkdir -p ${1}
     for run in 1 2 3 4 5
     do
-        curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | tar xzvf - --wildcards --directory=${1} --strip-components=1 "*/docs/spec/*"
+        if [ -x "$(command -v gtar)" ]; then
+            curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | gtar xzvf - --wildcards --directory=${1} --strip-components=1 "*/docs/spec/*"
+        else
+            curl --silent --fail https://codeload.github.com/elastic/apm-server/tar.gz/${2} | tar xzvf - --wildcards --directory=${1} --strip-components=1 "*/docs/spec/*"
+        fi
         result=$?
         if [ $result -eq 0 ]; then break; fi
         sleep 1
