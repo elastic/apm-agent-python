83017f6b63093bc92c6151979c7b748071ee5184
Colton Myers <colton.myers@gmail.com>
2019-10-07 12:29:50 -0600

    Add id=None to DroppedSpan for consistency

--- elasticapm/traces.py
+++ elasticapm/traces.py
@@ -451,6 +451,7 @@ class DroppedSpan(BaseSpan):
     def __init__(self, parent, leaf=False):
         self.parent = parent
         self.leaf = leaf
+        self.id = None
         super(DroppedSpan, self).__init__()
 
     def end(self, skip_frames=0, duration=None):
