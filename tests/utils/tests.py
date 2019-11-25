dd3537a98cabaca4dfb38333eebda062a7613fd6
Benjamin Wohlwend <beni@elastic.co>
2019-11-25 10:14:47 +0100

    update changelog for #649 (#650)
    also, use proper parametrization for test_get_url_dict test cases

--- tests/utils/tests.py
+++ tests/utils/tests.py
@@ -52,61 +52,79 @@ def test_deprecation():
     deprecated_function()
 
 
-def test_get_url_dict():
-    data = {
-        "http://example.com": {
-            "protocol": "http:",
-            "hostname": "example.com",
-            "pathname": "",
-            "full": "http://example.com",
-        },
-        "http://example.com:443": {
-            "protocol": "http:",
-            "hostname": "example.com",
-            "port": "443",
-            "pathname": "",
-            "full": "http://example.com:443",
-        },
-        "http://example.com:443/a/b/c": {
-            "protocol": "http:",
-            "hostname": "example.com",
-            "port": "443",
-            "pathname": "/a/b/c",
-            "full": "http://example.com:443/a/b/c",
-        },
-        "https://example.com:443/": {
-            "protocol": "https:",
-            "hostname": "example.com",
-            "port": "443",
-            "pathname": "/",
-            "full": "https://example.com:443/",
-        },
-        "https://example.com:443/a/b/c?de": {
-            "protocol": "https:",
-            "hostname": "example.com",
-            "port": "443",
-            "pathname": "/a/b/c",
-            "search": "?de",
-            "full": "https://example.com:443/a/b/c?de",
-        },
-        "https://[::ffff:a9fe:a9fe]/a/b/c?de": {
-            "protocol": "https:",
-            "hostname": "::ffff:a9fe:a9fe",
-            "pathname": "/a/b/c",
-            "search": "?de",
-            "full": "https://[::ffff:a9fe:a9fe]/a/b/c?de",
-        },
-        "http://[::ffff:a9fe:a9fe]:80/a/b/c?de": {
-            "protocol": "http:",
-            "hostname": "::ffff:a9fe:a9fe",
-            "port": "80",
-            "pathname": "/a/b/c",
-            "search": "?de",
-            "full": "http://[::ffff:a9fe:a9fe]:80/a/b/c?de",
-        },
-    }
-    for url, expected in data.items():
-        assert get_url_dict(url) == expected
+@pytest.mark.parametrize(
+    "url,expected",
+    [
+        (
+            "http://example.com",
+            {"protocol": "http:", "hostname": "example.com", "pathname": "", "full": "http://example.com"},
+        ),
+        (
+            "http://example.com:443",
+            {
+                "protocol": "http:",
+                "hostname": "example.com",
+                "port": "443",
+                "pathname": "",
+                "full": "http://example.com:443",
+            },
+        ),
+        (
+            "http://example.com:443/a/b/c",
+            {
+                "protocol": "http:",
+                "hostname": "example.com",
+                "port": "443",
+                "pathname": "/a/b/c",
+                "full": "http://example.com:443/a/b/c",
+            },
+        ),
+        (
+            "https://example.com:443/",
+            {
+                "protocol": "https:",
+                "hostname": "example.com",
+                "port": "443",
+                "pathname": "/",
+                "full": "https://example.com:443/",
+            },
+        ),
+        (
+            "https://example.com:443/a/b/c?de",
+            {
+                "protocol": "https:",
+                "hostname": "example.com",
+                "port": "443",
+                "pathname": "/a/b/c",
+                "search": "?de",
+                "full": "https://example.com:443/a/b/c?de",
+            },
+        ),
+        (
+            "https://[::ffff:a9fe:a9fe]/a/b/c?de",
+            {
+                "protocol": "https:",
+                "hostname": "::ffff:a9fe:a9fe",
+                "pathname": "/a/b/c",
+                "search": "?de",
+                "full": "https://[::ffff:a9fe:a9fe]/a/b/c?de",
+            },
+        ),
+        (
+            "http://[::ffff:a9fe:a9fe]:80/a/b/c?de",
+            {
+                "protocol": "http:",
+                "hostname": "::ffff:a9fe:a9fe",
+                "port": "80",
+                "pathname": "/a/b/c",
+                "search": "?de",
+                "full": "http://[::ffff:a9fe:a9fe]:80/a/b/c?de",
+            },
+        ),
+    ],
+)
+def test_get_url_dict(url, expected):
+    assert get_url_dict(url) == expected
 
 
 def test_get_name_from_func():
