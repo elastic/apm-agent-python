0c57059e9843a72366712b90ed88e85e647a27c4
DESKTOP-HJUTBFV\timp6 <wpenn@csod.com>
2020-02-20 00:31:28 -0800

    Adding missing parameters, and executing pytest via python

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -2,4 +2,4 @@ call .\tests\scripts\install_chocolatey.bat
 call .\tests\scripts\install_python.bat %1
 call .\tests\scripts\install_modules.bat %2
 call .\tests\scripts\download_json_schema.bat
-pytest
+python -m pytest
