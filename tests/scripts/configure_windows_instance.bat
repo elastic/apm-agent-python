37c6b9e7cb588d3f9d8beed43b3b92334a7c64c6
elwpenn <william.penn@elastic.co>
2020-02-25 01:14:05 -0800

    Replacing install module bat with using requirements file

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -4,7 +4,7 @@ call .\tests\scripts\install_python.bat %1
 call move c:\%2\python.exe c:\%2\%2.exe
 call refreshenv
 call %2 --version
-call .\tests\scripts\install_modules.bat %2
+call %2 -m pip -install -r .\tests\requirements\requirements-base.txt
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
 call %2 -m pytest -vv
