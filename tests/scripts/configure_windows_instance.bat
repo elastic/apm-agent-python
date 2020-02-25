3144df8df51223e869ddd88fcb91b202c6845dc5
elwpenn <william.penn@elastic.co>
2020-02-25 10:47:33 -0800

    Removing version call

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -3,7 +3,6 @@ call refreshenv
 call .\tests\scripts\install_python.bat %1
 call move c:\%2\python.exe c:\%2\%2.exe
 call refreshenv
-call %2 --version
 call %2 -m pip install -r .\tests\requirements\requirements-base.txt
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
