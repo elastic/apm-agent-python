6c7c36c19841e3fbb280dda851c8b6612cac3e95
DESKTOP-HJUTBFV\timp6 <wpenn@csod.com>
2020-02-24 22:03:35 -0800

    Adding gherkin feature download

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -4,4 +4,5 @@ call .\tests\scripts\install_python.bat %1
 call refreshenv
 call .\tests\scripts\install_modules.bat
 call .\tests\scripts\download_json_schema.bat
+call .\tests\scripts\download_gherkin_features.bat
 call py -m pytest
