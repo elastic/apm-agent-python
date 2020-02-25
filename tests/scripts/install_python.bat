2731a3850e937471c7479c364be6bb16be7530fc
elwpenn <william.penn@elastic.co>
2020-02-25 01:06:46 -0800

    Forcing python to re-install via choco

--- tests/scripts/install_python.bat
+++ tests/scripts/install_python.bat
@@ -3,6 +3,6 @@ choco uninstall python3 -y
 set pythonVersion=%1
 set pythonVersion=!pythonVersion:~0,1!
 echo !pythonVersion!
-if %pythonVersion% EQU 3 (choco install python3 --version=%1 -y) else (choco install python2 -y)
+if %pythonVersion% EQU 3 (choco install python3 --version=%1 -y --force) else (choco install python2 -y --force)
 SETLOCAL DisableDelayedExpansion
 
