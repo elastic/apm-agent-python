1d2030c38ab8ad26389daedfc172521ca6ace8de
elwpenn <william.penn@elastic.co>
2020-02-25 01:24:14 -0800

    Removing hyphen from pip install command

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -1,10 +1,10 @@
 call .\tests\scripts\install_chocolatey.bat
 call refreshenv
 call .\tests\scripts\install_python.bat %1
-call move c:\%2\python.exe c:\%2\%2.exe
+call move c:\%2\python.exe c:\%2\%2.exe /Y
 call refreshenv
 call %2 --version
-call %2 -m pip -install -r .\tests\requirements\requirements-base.txt
+call %2 -m pip install -r .\tests\requirements\requirements-base.txt
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
 call %2 -m pytest -vv
