8525ac90021b9e2acad3d9c544a8110f05c0b3a0
DESKTOP-HJUTBFV\timp6 <wpenn@csod.com>
2020-02-25 00:20:06 -0800

    Adding -vv to pytest to triage skipped tests

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -2,7 +2,8 @@ call .\tests\scripts\install_chocolatey.bat
 call refreshenv
 call .\tests\scripts\install_python.bat %1
 call refreshenv
+call py --version
 call .\tests\scripts\install_modules.bat
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
-call py -m pytest
+call py -m pytest -vv
