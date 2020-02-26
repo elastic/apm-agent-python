07027a1d5ffdd299baff3e29ba11ef3b5fcf5e0b
elwpenn <william.penn@elastic.co>
2020-02-25 16:12:56 -0800

    fixing switch in pip install

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -2,7 +2,7 @@ call .\tests\scripts\install_chocolatey.bat
 call refreshenv
 call .\tests\scripts\install_python.bat %1 %2
 call refreshenv
-call .\tests\scripts\install_modules.bat %1 %2
+call %2 -m pip install -r .\tests\requirements\requirements-base.txt
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
 call .\tests\scripts\execute_pytest_windows.bat %1 %2
--- tests/scripts/install_modules.bat
+++ tests/scripts/install_modules.bat
@@ -3,5 +3,5 @@ set pythonVersion=%1
 set pythonExecutable=%2
 set majorVersion=!pythonVersion:~0,1!
 echo !majorVersion!
-if %majorVersion% EQU 2 (!pythonExecutable! -m pip install -r .\tests\requirements\requirements-base.txt) else (!pythonExecutable! -m pip install /r .\tests\requirements\requirements-base.txt)
+if %majorVersion% EQU 2 (!pythonExecutable! -m pip install -r .\tests\requirements\requirements-base.txt) else (!pythonExecutable! -m pip install -r .\tests\requirements\requirements-base.txt)
 SETLOCAL DisableDelayedExpansion
