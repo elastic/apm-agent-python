ffae86423bfc2858d3189069a26bed2a398f9611
elwpenn <william.penn@elastic.co>
2020-02-25 16:56:34 -0800

    Modifying batch to not mask failures

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -1,8 +1,8 @@
-call .\tests\scripts\install_chocolatey.bat
-call refreshenv
-call .\tests\scripts\install_python.bat %1 %2
-call refreshenv
-call %2 -m pip install -r .\tests\requirements\requirements-base.txt
-call .\tests\scripts\download_json_schema.bat
-call .\tests\scripts\download_gherkin_features.bat
-.\tests\scripts\execute_pytest_windows.bat %1 %2
+.\tests\scripts\install_chocolatey.bat^
+    && refreshenv^
+    && .\tests\scripts\install_python.bat %1 %2^
+    && refreshenv^
+    && %2 -m pip install -r .\tests\requirements\requirements-base.txt^
+    && .\tests\scripts\download_json_schema.bat^
+    && .\tests\scripts\download_gherkin_features.bat^
+    && .\tests\scripts\execute_pytest_windows.bat %1 %2
