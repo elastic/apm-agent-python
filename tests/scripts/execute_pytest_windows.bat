7fdb816c58793c95c2990e1a11f3d4acb3e42056
elwpenn <william.penn@elastic.co>
2020-02-26 06:10:45 -0800

    Removing closing SETLOCAL DisableDelayedExpansion statement to prevent losing error code from previous statement

--- tests/scripts/execute_pytest_windows.bat
+++ tests/scripts/execute_pytest_windows.bat
@@ -5,4 +5,3 @@ set majorVersion=!pythonVersion:~0,1!
 set minorVersion=!pythonVersion:~2,1!
 if %majorVersion% EQU 2 (set pyArgs=--ignore-glob="*/py3_*.py" --ignore-glob="*/asyncio/*") else ( if %minorVersion% EQU 5 (set pyArgs=--ignore-glob="*/asyncio/*"))
 %pythonExecutable% -m pytest --cov --cov-report xml:coverage.xml %pyArgs%
-SETLOCAL DisableDelayedExpansion
