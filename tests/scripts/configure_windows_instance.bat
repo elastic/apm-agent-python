2522d9dfc4e21f5271ed52e29eb66117b59f8173
elwpenn <william.penn@elastic.co>
2020-02-25 17:36:26 -0800

    Getting contents of python directory for debugging

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -2,7 +2,7 @@
     && refreshenv^
     && .\tests\scripts\install_python.bat %1 %2^
     && refreshenv^
-    && dir c:\
+    && dir c:\%2^
     && %2 -m pip install -r .\tests\requirements\requirements-base.txt^
     && .\tests\scripts\download_json_schema.bat^
     && .\tests\scripts\download_gherkin_features.bat^
