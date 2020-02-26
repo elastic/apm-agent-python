c8d4f4877fb9e73f2ec8e2afcf39bc630086eeab
elwpenn <william.penn@elastic.co>
2020-02-25 16:31:46 -0800

    Removing call from executing pytest so failures are caught when tests don't run or fail

--- tests/scripts/configure_windows_instance.bat
+++ tests/scripts/configure_windows_instance.bat
@@ -5,4 +5,4 @@ call refreshenv
 call %2 -m pip install -r .\tests\requirements\requirements-base.txt
 call .\tests\scripts\download_json_schema.bat
 call .\tests\scripts\download_gherkin_features.bat
-call .\tests\scripts\execute_pytest_windows.bat %1 %2
+.\tests\scripts\execute_pytest_windows.bat %1 %2
