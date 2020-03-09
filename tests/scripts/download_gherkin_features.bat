1d73c5e325839a13b5278d3f82da43994d91b69b
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-09 17:15:32 +0000

    Cherry-pick with some minor changes from: https://github.com/elastic/apm-agent-python/blob/f3061652e993ba4fc36d8bb6dcfa636018a3592d/tests/scripts/download_gherkin_features.bat

---
+++ tests/scripts/download_gherkin_features.bat
@@ -0,0 +1,7 @@
+: Adding gherkin feature download for windows
+mkdir .\tests\tempFeatures
+curl https://codeload.github.com/elastic/apm/zip/master -o .\tests\tempFeatures\features.zip
+7z x .\tests\tempFeatures\features.zip -o.\tests\tempFeatures *.feature -y -r
+mkdir .\tests\bdd\features
+xcopy .\tests\tempFeatures\apm-master\tests\agents\gherkin-specs\* .\tests\bdd\features /Y /S
+del .\tests\tempFeatures\ /F /Q /S
