5bbfb37ff74a5377f054e9fa8bda624549384525
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-11 15:13:07 +0000

    ci(jenkins): simplify the scripts

--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -10,6 +10,6 @@ ECHO Download gherkin feature dependencies
 call .\tests\scripts\download_gherkin_features.bat
 
 set PYTEST_JUNIT="--junitxml=.\tests\python-agent-junit.xml"
-if "%ASYNCIO%" == "true" call .\tests\appveyor\build.cmd %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% -m "not integrationtest"
-if "%ASYNCIO%" == "false" call .\tests\appveyor\build.cmd %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*/asyncio/*" -m "not integrationtest"
-call .\tests\appveyor\build.cmd %PYTHON%\python.exe setup.py bdist_wheel
+if "%ASYNCIO%" == "true" call %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% -m "not integrationtest"
+if "%ASYNCIO%" == "false" call %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*/asyncio/*" -m "not integrationtest"
+call %PYTHON%\python.exe setup.py bdist_wheel
