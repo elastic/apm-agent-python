43f77293c1f94564a9b98b1b3e1f0657294e0926
Benjamin Wohlwend <beni@elastic.co>
2020-04-09 15:47:26 +0200

    undo some unnecessary changes

--- tests/instrumentation/pymongo_tests.py
+++ tests/instrumentation/pymongo_tests.py
@@ -34,15 +34,14 @@ import os
 import pytest
 
 from elasticapm.conf.constants import TRANSACTION
-from elasticapm.instrumentation.packages.pymongo import get_destination_from_client
 
 pymongo = pytest.importorskip("pymongo")
 
 
 pytestmark = [pytest.mark.mongodb]
 
-# if "MONGODB_HOST" not in os.environ:
-#     pytestmark.append(pytest.mark.skip("Skipping mongodb tests, no MONGODB_HOST environment variable set"))
+if "MONGODB_HOST" not in os.environ:
+    pytestmark.append(pytest.mark.skip("Skipping mongodb tests, no MONGODB_HOST environment variable set"))
 
 
 @pytest.fixture()
