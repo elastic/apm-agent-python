45be344697698907b9e6f12018608d2303e365a6
Ryan.K <ryankung@ieee.org>
2020-06-08 17:36:52 +0800

    setup name

--- elasticapm/instrumentation/packages/graphql.py
+++ elasticapm/instrumentation/packages/graphql.py
@@ -80,7 +80,7 @@ class GraphQLBackendInstrumentation(AbstractInstrumentedModule):
     def get_graphql_tx_name(self, graphql_doc):
         op = graphql_doc.definitions[0].operation
         fields = graphql_doc.definitions[0].selection_set.selections
-        return "%s %s" % (op.upper(), "+".join([f.name.value for f in fields]))
+        return "GraphQL %s %s" % (op.upper(), "+".join([f.name.value for f in fields]))
 
     def call(self, module, method, wrapped, instance, args, kwargs):
         graphql_document = wrapped(*args, **kwargs)
