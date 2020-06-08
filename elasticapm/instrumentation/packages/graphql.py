b7f13e755127d70af79c64ecf26efa61ae374cfe
Ryan.K <ryankung@ieee.org>
2020-06-08 17:03:33 +0800

    rm blank line

--- elasticapm/instrumentation/packages/graphql.py
+++ elasticapm/instrumentation/packages/graphql.py
@@ -77,12 +77,10 @@ class GraphQLBackendInstrumentation(AbstractInstrumentedModule):
         ("graphql.backend.cache", "GraphQLCachedBackend.document_from_string"),
     ]
 
-
     def get_graphql_tx_name(self, graphql_doc):
         op = graphql_doc.definitions[0].operation
         fields = graphql_doc.definitions[0].selection_set.selections
         return "%s %s" % (op.upper(), "+".join([f.name.value for f in fields]))
-
 
     def call(self, module, method, wrapped, instance, args, kwargs):
         graphql_document = wrapped(*args, **kwargs)
