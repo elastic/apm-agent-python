7e938623f3a1ba0afa7cac57a7aa2c8da59b7e15
Aumit Leon <AumitLeon@users.noreply.github.com>
2020-06-26 18:53:28 -0400

    Config sanitize field names (#860)
    * feat: configurable SANITIZE_FIELD_NAMES
    
    * tests: tests for configurable SANITIZE_FIELD_NAMES
    
    * refactor: wrap a class around field name operations
    
    * chore: fixes
    
    * Send client, not transport, to processors
    
    * fix: update sanitize_stacktrace_locals to use config fields
    
    * Cleanup processors
    
    Co-authored-by: Colton Myers <colton.myers@gmail.com>
    
    * fix: pass sanitize_field_names to _sanitize_string
    
    * chore: cleanup, tests, and doc updates
    
    * chore: reorder docs statements
    
    Co-authored-by: Colton Myers <colton.myers@gmail.com>

--- elasticapm/conf/__init__.py
+++ elasticapm/conf/__init__.py
@@ -35,6 +35,7 @@ import re
 import socket
 import threading
 
+from elasticapm.conf.constants import BASE_SANITIZE_FIELD_NAMES
 from elasticapm.utils import compat, starmatch_to_regex
 from elasticapm.utils.logging import get_logger
 from elasticapm.utils.threading import IntervalTimer, ThreadManager
@@ -286,6 +287,7 @@ class Config(_ConfigBase):
             "elasticapm.processors.sanitize_http_request_body",
         ],
     )
+    sanitize_field_names = _ListConfigValue("SANITIZE_FIELD_NAMES", default=BASE_SANITIZE_FIELD_NAMES)
     metrics_sets = _ListConfigValue(
         "METRICS_SETS",
         default=[
