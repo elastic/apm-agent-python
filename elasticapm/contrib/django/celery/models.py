5760bcf66b1909bd44f99731acdc518ba5e0ec10
Jaap Roes <jaap.roes@gmail.com>
2020-07-02 21:52:30 +0200

    s/sentry/elasticapm

--- elasticapm/contrib/django/celery/models.py
+++ elasticapm/contrib/django/celery/models.py
@@ -34,5 +34,5 @@ from django.core.exceptions import ImproperlyConfigured
 
 if "djcelery" not in settings.INSTALLED_APPS:
     raise ImproperlyConfigured(
-        "Put 'djcelery' in your INSTALLED_APPS setting in order to use the sentry celery client."
+        "Put 'djcelery' in your INSTALLED_APPS setting in order to use the elasticapm celery client."
     )
