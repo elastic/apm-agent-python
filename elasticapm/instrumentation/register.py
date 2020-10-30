31de706f2cedae17bde543f81a4413fa0edbb39d
root <root@ip-172-31-86-11.us-west-2.compute.internal>
2020-10-30 13:09:06 +0000

    kafka instrumentation native support for apm-python agent

--- elasticapm/instrumentation/register.py
+++ elasticapm/instrumentation/register.py
@@ -64,6 +64,7 @@ _cls_register = {
     "elasticapm.instrumentation.packages.graphql.GraphQLExecutorInstrumentation",
     "elasticapm.instrumentation.packages.graphql.GraphQLBackendInstrumentation",
     "elasticapm.instrumentation.packages.httpcore.HTTPCoreInstrumentation",
+    "elasticapm.instrumentation.packages.kafka.KafkaInstrumentation",
 }
 
 if sys.version_info >= (3, 7):
