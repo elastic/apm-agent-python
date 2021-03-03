ea1731a9d31bbbf1b373e748c2b14551c509beeb
M. Utku Altinkaya <utkualtinkaya@gmail.com>
2021-03-03 13:20:08 +0300

    Redis Cluster Instrumentation
    Added Redis Cluster Instrumentation Package

---
+++ elasticapm/instrumentation/packages/rediscluster.py
@@ -0,0 +1,85 @@
+#  BSD 3-Clause License
+#
+#  Copyright (c) 2019, Elasticsearch BV
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  * Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import
+
+from elasticapm.instrumentation.packages.base import AbstractInstrumentedModule
+from elasticapm.traces import capture_span, execution_context
+
+
+class RedisClusterInstrumentation(AbstractInstrumentedModule):
+    name = "rediscluster"
+
+    # no need to instrument StrictRedis in redis-py >= 3.0
+    instrument_list = [("rediscluster.client", "RedisCluster.execute_command")]
+
+    def call(self, module, method, wrapped, instance, args, kwargs):
+        if len(args) > 0:
+            wrapped_name = str(args[0])
+        else:
+            wrapped_name = self.get_wrapped_name(wrapped, instance, method)
+
+        with capture_span(wrapped_name, span_type="db", span_subtype="rediscluster", span_action="query", leaf=True):
+            return wrapped(*args, **kwargs)
+
+
+class RedisClusterPipelineInstrumentation(AbstractInstrumentedModule):
+    name = "rediscluster"
+
+    # BasePipeline has been renamed to Pipeline in redis-py 3
+    instrument_list = [("rediscluster.pipeline", "ClusterPipeline.execute")]
+
+    def call(self, module, method, wrapped, instance, args, kwargs):
+        wrapped_name = self.get_wrapped_name(wrapped, instance, method)
+        with capture_span(wrapped_name, span_type="db", span_subtype="rediscluster", span_action="query", leaf=True):
+            return wrapped(*args, **kwargs)
+
+
+class RedisClusterConnectionInstrumentation(AbstractInstrumentedModule):
+    name = "rediscluster"
+
+    instrument_list = (("redis.connection", "Connection.send_packed_command"),)
+
+    def call(self, module, method, wrapped, instance, args, kwargs):
+        span = execution_context.get_span()
+        if span and span.subtype == "rediscluster":
+            span.context["destination"] = get_destination_info(instance)
+        return wrapped(*args, **kwargs)
+
+
+def get_destination_info(connection):
+    destination_info = {"service": {"name": "rediscluster", "resource": "rediscluster", "type": "db"}}
+    if hasattr(connection, "port"):
+        destination_info["port"] = connection.port
+        destination_info["address"] = connection.host
+    elif hasattr(connection, "path"):
+        destination_info["port"] = None
+        destination_info["address"] = "unix://" + connection.path
+    return destination_info
