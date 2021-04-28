42be89346f80c30c926b120a0514e2ac81ab8700
Benjamin Wohlwend <beni@elastic.co>
2021-04-28 09:42:23 +0200

    try to use a virtualenv on Windows to fix dirty environment issues

--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -6,15 +6,17 @@
 @echo off
 
 @echo on
+set VENV_PYTHON=%cd%\venv\bin\
+
 set COVERAGE_FILE=.coverage.%VERSION%.%WEBFRAMEWORK%
 set IGNORE_PYTHON3_WITH_PYTHON2=
 if "%VERSION%" == "2.7" set IGNORE_PYTHON3_WITH_PYTHON2=--ignore-glob="*\py3_*.py"
 
 set PYTEST_JUNIT="--junitxml=.\tests\python-agent-junit.xml"
 if "%ASYNCIO%" == "true" (
-    %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
 if "%ASYNCIO%" == "false" (
-    %PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
-call %PYTHON%\python.exe setup.py bdist_wheel
+call %VENV_PYTHON%\python.exe setup.py bdist_wheel
