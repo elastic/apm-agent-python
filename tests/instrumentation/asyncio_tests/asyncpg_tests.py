a64089310b3136305d997b90dc0341a14472e08f
Stefan Tjarks <66305+stj@users.noreply.github.com>
2021-11-24 11:35:47 -0800

    Shorten long SQL query to prevent oversized event
    APM server allows events with a max size of 10000 characters. Shorten
    queries traced in asyncpg to max allowed.
    
    See: #842

--- tests/instrumentation/asyncio_tests/asyncpg_tests.py
+++ tests/instrumentation/asyncio_tests/asyncpg_tests.py
@@ -139,3 +139,17 @@ async def test_fetch_methods(connection, elasticapm_client, method, verify):
     assert span["action"] == "query"
     assert span["sync"] is False
     assert span["name"] == "SELECT FROM test"
+
+
+async def test_truncate_long_sql(connection, elasticapm_client):
+    elasticapm_client.begin_transaction("test")
+    await connection.execute(
+        f"SELECT id, username FROM test WHERE username = {'x' * 10010};"
+    )
+    elasticapm_client.end_transaction("test", "OK")
+
+    transactions = elasticapm_client.events[constants.TRANSACTION]
+    spans = elasticapm_client.spans_for_transaction(transactions[0])
+
+    assert len(spans[0]["context"]["db"]["statement"]) == 10000
+    assert spans[0]["context"]["db"]["statement"].endswith("...")
