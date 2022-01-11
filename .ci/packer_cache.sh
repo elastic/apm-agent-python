ed009e2ebaad62d375a40ad71d9212fb437b5864
Benjamin Wohlwend <beni@elastic.co>
2022-01-11 10:45:41 +0100

    use a limited amount of Python versions for PR runs
    the CI runs on PRs are getting prohibitively long. To work around
    this, we can use only a limited set of Python versions for PR runs
    and do a full, nightly run (the same approach we already use for
    framework versions).

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -3,7 +3,7 @@
 source /usr/local/bin/bash_standard_lib.sh
 
 if [ -x "$(command -v docker)" ]; then
-    grep "-" .ci/.jenkins_python.yml | cut -d'-' -f2- | \
+    grep "-" .ci/.jenkins_python_full.yml | cut -d'-' -f2- | \
     while read -r version;
     do
         imageName="apm-agent-python:${version}"
