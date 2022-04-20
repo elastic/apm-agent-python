6541478fd1e0521a47852f95fb8efacea14f4a70
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-04-20 16:03:01 +0100

    packer_cache: avoid failing

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -6,9 +6,13 @@ if [ -x "$(command -v docker)" ]; then
     grep "-" .ci/.jenkins_python_full.yml | cut -d'-' -f2- | \
     while read -r version;
     do
-        imageName="apm-agent-python:${version}"
-        registryImageName="docker.elastic.co/observability-ci/${imageName}"
-        (retry 2 docker pull "${registryImageName}") || echo 'Skip failing packer_cache'
-        docker tag "${registryImageName}" "${imageName}"
+        if [[ "$version" == *"#"* ]]; then
+            echo "skipped ${version}"
+        else
+            imageName="apm-agent-python:${version}"
+            registryImageName="docker.elastic.co/observability-ci/${imageName}"
+            (retry 2 docker pull "${registryImageName}") || echo 'Skip failing packer_cache'
+            docker tag "${registryImageName}" "${imageName}" || echo 'Skip failing packer_cache'
+        fi
     done
 fi
