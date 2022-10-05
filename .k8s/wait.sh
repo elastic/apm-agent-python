58bf868e80649e3b7002dd4eaa54186568df4e31
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-10-05 22:04:41 +0100

    wait: cosmetic

--- .k8s/wait.sh
+++ .k8s/wait.sh
@@ -28,6 +28,8 @@ function finish {
     do
         echo -e "    ${BLUE}Logs for the container $CONTAINER can be found in $BUILD_OUTPUT_DIR/$CONTAINER.out${NC}"
         kubectl logs $POD --container $CONTAINER $NAMESPACE > $BUILD_OUTPUT_DIR/$CONTAINER.out
+        #echo -e "    ${BLUE}JUnit for the container $CONTAINER can be found in $BUILD_OUTPUT_DIR/$CONTAINER-junit.xml${NC}"
+        #echo "kubectl cp --container $CONTAINER $NAMESPACE $POD:/code/tests/python-agent-junit.xml $BUILD_OUTPUT_DIR/$CONTAINER-junit.xml"
     done
     echo -e "    ${GREEN}Exported${NC}"
 }
@@ -36,6 +38,7 @@ trap finish ERR EXIT
 function waitForJobsToStart {
     jobcount=0
     while [ $jobcount -eq 0 -o $jobcount -ne $(countTestJobs) ]; do
+        echo -n "."
         jobcount=$(countTestJobs)
         sleep 5
     done
@@ -45,18 +48,18 @@ function waitForJobsToStart {
 # See https://stackoverflow.com/questions/55073453/wait-for-kubernetes-job-to-complete-on-either-failure-success-using-command-line
 function waitForCompletion {
     while true; do
-        echo -e "    ${BLUE}is job completed/failed?${NC}"
-        if kubectl wait job.batch --for=condition=complete --timeout=0 $BATCH_FILTER $NAMESPACE 2>/dev/null; then
+        echo -n "."
+        if kubectl wait job.batch --for=condition=complete --timeout=0 $BATCH_FILTER $NAMESPACE &>/dev/null; then
             job_result=0
             break
         fi
 
-        if kubectl wait job.batch --for=condition=failed --timeout=0 $BATCH_FILTER $NAMESPACE 2>/dev/null; then
+        if kubectl wait job.batch --for=condition=failed --timeout=0 $BATCH_FILTER $NAMESPACE &>/dev/null; then
             job_result=1
             break
         fi
 
-        sleep 10
+        sleep 5
     done
 
     if [[ $job_result -eq 0 ]]; then
