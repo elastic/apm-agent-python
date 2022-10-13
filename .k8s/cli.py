ddc792b0f0a95f2e429422a4b143eb9aeb5d7f79
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-10-13 14:30:46 +0100

    add time

--- .k8s/cli.py
+++ .k8s/cli.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import click
 import commands
+import time
+
 
 @click.group()
 def main():
@@ -13,4 +15,8 @@ main.add_command(commands.test)
 main.add_command(commands.results)
 
 if __name__ == '__main__':
-    main()
+    start_time = time.time()
+    try:
+        main()
+    finally:
+        click.echo(click.style("Command took %.1f seconds" % (time.time() - start_time), fg='white'))
