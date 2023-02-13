611b586152ae1396e1394a40d1216204bd6bb999
Jan Calanog <jan.calanog@elastic.co>
2023-02-13 09:08:08 +0100

    Required Status Check (#1749)
    * Create single status check that can be set as required
    
    * Let windows test runs exit with the correct exit code
    
    * Set the exit code
    
    * Formatting
    
    * fix
    
    * More readable jq query

--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -14,9 +14,9 @@ if "%VERSION%" == "2.7" set IGNORE_PYTHON3_WITH_PYTHON2=--ignore-glob="*\py3_*.p
 
 set PYTEST_JUNIT="--junitxml=.\tests\windows-%VERSION%-%FRAMEWORK%-%ASYNCIO%-python-agent-junit.xml"
 if "%ASYNCIO%" == "true" (
-    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || cmd /c exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
 if "%ASYNCIO%" == "false" (
-    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || cmd /c exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
 call %VENV_PYTHON%\python.exe setup.py bdist_wheel
