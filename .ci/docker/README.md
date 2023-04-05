a322f47cb4eda7c59258ad466bd6e6f246b69a43
Jan Calanog <jan.calanog@elastic.co>
2023-04-05 09:24:45 +0200

    Add utility script for building and pushing docker images

---
+++ .ci/docker/README.md
@@ -0,0 +1,33 @@
+# apm-agent-python docker images for testing
+
+Utility script for building and pushing the images based on `.ci/.matrix_python_full.yml`.
+
+## Options
+
+| Name     | Description                                             |
+|----------|---------------------------------------------------------|
+| name     | Name of the docker image. (<registry>/<name>:<tag>)     |
+| registry | Registry of the docker image. (<registry>/<name>:<tag>) |
+| action   | Either `build` or `push`.                               |
+
+## Usage
+
+The script must be executed from the repository root.
+
+### Build
+
+To build the images run
+
+```bash
+  ./util.sh --action build
+```
+
+You can set your own docker registry with the flag `--registry x.y.z/org`. The default is `elasticobservability.
+
+### Push
+
+To push the images run
+
+```bash
+  ./util.sh --action push
+```
---
+++ .ci/docker/util.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+project_root=$(dirname "$(dirname "$(dirname "$(realpath "$0" )")")")
+
+IMAGE_NAME="apm-agent-python-testing"
+REGISTRY="elasticobservability"
+
+while (( "$#" )); do
+  case "$1" in
+    -n|--name)
+      IMAGE_NAME=$2
+      shift 2
+      ;;
+    -r|--registry)
+      REGISTRY=$2
+      shift 2
+      ;;
+    -a|--action)
+      ACTION=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      shift
+      ;;
+  esac
+done
+
+versions=$(yq '.VERSION[]' "${project_root}/.ci/.matrix_python_full.yml")
+
+for version in $versions; do
+  case $ACTION in
+  build)
+    docker build \
+        -f "${project_root}/tests/Dockerfile" \
+        --build-arg PYTHON_IMAGE="${version/-/:}" \
+        -t "${REGISTRY}/${IMAGE_NAME}:${version}" \
+        "${project_root}/tests"
+    ;;
+  push)
+    docker push "${REGISTRY}/${IMAGE_NAME}:${version}"
+    ;;
+  esac
+done
