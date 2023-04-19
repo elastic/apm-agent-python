25eb517ee95607f940ea99c729db6cc4df00fe62
Colton Myers <colton@basepi.net>
2023-04-19 09:30:42 -0600

    Add support for partial transaction reporting in Lambda (#1784)
    * Add support for partial transaction reporting in Lambda
    
    * Fix typo 😅
    
    * Fix duration handling for partial transaction
    
    * Fix config access from copy pasta
    
    * Use the correct content-type
    
    * Ditch gzip encoding
    
    * Add early metadata send
    
    * Handle 404 for partial transactions
    
    * Fix global
    
    * Don't send early metadat on failed partial transaction
    
    * Send metadata to with the partial transaction
    
    * Update for new metadata spec
    
    * CHANGELOG
    
    * Add test
    
    * Review changes

--- elasticapm/traces.py
+++ elasticapm/traces.py
@@ -385,13 +385,16 @@ class Transaction(BaseSpan):
         return self.trace_parent.span_id
 
     def to_dict(self) -> dict:
-        self.context["tags"] = self.labels
+        # This copy() only covers top level `.pop()` calls, so if we ever start
+        # modifying nested data, we'll need to do a deep copy.
+        context = self.context.copy()
+        context["tags"] = self.labels
         result = {
             "id": self.id,
             "trace_id": self.trace_parent.trace_id,
             "name": encoding.keyword_field(self.name or ""),
             "type": encoding.keyword_field(self.transaction_type),
-            "duration": self.duration.total_seconds() * 1000,
+            "duration": self.duration.total_seconds() * 1000 if self.duration else None,
             "result": encoding.keyword_field(str(self.result)),
             "timestamp": int(self.timestamp * 1_000_000),  # microseconds
             "outcome": self.outcome,
@@ -419,26 +422,26 @@ class Transaction(BaseSpan):
         if self.links:
             result["links"] = self.links
         # faas context belongs top-level on the transaction
-        if "faas" in self.context:
-            result["faas"] = self.context.pop("faas")
+        if "faas" in context:
+            result["faas"] = context.pop("faas")
         # otel attributes and spankind need to be top-level
-        if "otel_spankind" in self.context:
-            result["otel"] = {"span_kind": self.context.pop("otel_spankind")}
+        if "otel_spankind" in context:
+            result["otel"] = {"span_kind": context.pop("otel_spankind")}
         # Some transaction_store_tests use the Tracer without a Client -- the
         # extra check against `get_client()` is here to make those tests pass
         if elasticapm.get_client() and elasticapm.get_client().check_server_version(gte=(7, 16)):
-            if "otel_attributes" in self.context:
+            if "otel_attributes" in context:
                 if "otel" not in result:
-                    result["otel"] = {"attributes": self.context.pop("otel_attributes")}
+                    result["otel"] = {"attributes": context.pop("otel_attributes")}
                 else:
-                    result["otel"]["attributes"] = self.context.pop("otel_attributes")
+                    result["otel"]["attributes"] = context.pop("otel_attributes")
         else:
             # Attributes map to labels for older versions
-            attributes = self.context.pop("otel_attributes", {})
+            attributes = context.pop("otel_attributes", {})
             for key, value in attributes.items():
                 result["context"]["tags"][key] = value
         if self.is_sampled:
-            result["context"] = self.context
+            result["context"] = context
         return result
 
     def track_span_duration(self, span_type, span_subtype, self_duration):
--- elasticapm/transport/http.py
+++ elasticapm/transport/http.py
@@ -70,19 +70,22 @@ class Transport(HTTPTransportBase):
         self._http = None
         self._url = url
 
-    def send(self, data, forced_flush=False):
+    def send(self, data, forced_flush=False, custom_url=None, custom_headers=None):
         response = None
 
         headers = self._headers.copy() if self._headers else {}
         headers.update(self.auth_headers)
-        headers.update(
-            {
-                b"Content-Type": b"application/x-ndjson",
-                b"Content-Encoding": b"gzip",
-            }
-        )
+        if custom_headers:
+            headers.update(custom_headers)
+        else:
+            headers.update(
+                {
+                    b"Content-Type": b"application/x-ndjson",
+                    b"Content-Encoding": b"gzip",
+                }
+            )
 
-        url = self._url
+        url = custom_url or self._url
         if forced_flush:
             url = f"{url}?flushed=true"
         try:
@@ -146,7 +149,7 @@ class Transport(HTTPTransportBase):
                     }
                 }
         :return: a three-tuple of new version, config dictionary and validity in seconds.
-                 Any element of the tuple can be None.
+            Any element of the tuple can be None.
         """
         url = self._config_url
         data = json_encoder.dumps(keys).encode("utf-8")
