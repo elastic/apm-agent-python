81a693ab81dabb4dbecf232a632065afe6e2e37a
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-05-10 11:50:50 +0200

    github-actions: use for microbenchmark

---
+++ .ci/bench.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+## Buildkite specific configuration
+if [ "$CI" == "true" ] ; then
+	# If HOME is not set then use the current directory
+	# that's normally happening when running in the CI
+	# owned by Elastic.
+	if [ -z "$HOME" ] ; then
+		HOME=$(realpath ~)
+		export HOME
+	fi
+fi
+
+## Bench specific
+LANG='C.UTF-8'
+LC_ALL="${LANG}"
+export LANG
+export LC_ALL
+
+./scripts/run-benchmarks.sh "$(realpath ~)" "${APM_AGENT_GO_ES_URL}" "${APM_AGENT_GO_ES_USER}" "${APM_AGENT_GO_ES_PASS}"
