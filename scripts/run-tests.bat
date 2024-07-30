c2b6d376dc61d8f137f4e45e89a2629571e50517
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-07-30 09:47:22 +0200

    Drop `setup.py test` (#2090)
    * scripts/run-tests: remove python2 relics
    
    * setup.py: drop test
    
    For compatibility with setuptools>=72 that remove the test command.
    
    See https://github.com/pypa/setuptools/issues/931
    
    * setup.py: remove unused imports

--- scripts/run-tests.bat
+++ scripts/run-tests.bat
@@ -9,14 +9,12 @@
 set VENV_PYTHON=%cd%\venv\Scripts\
 
 set COVERAGE_FILE=.coverage.windows.%VERSION%.%FRAMEWORK%.%ASYNCIO%
-set IGNORE_PYTHON3_WITH_PYTHON2=
-if "%VERSION%" == "2.7" set IGNORE_PYTHON3_WITH_PYTHON2=--ignore-glob="*\py3_*.py"
 
 set PYTEST_JUNIT="--junitxml=.\tests\windows-%VERSION%-%FRAMEWORK%-%ASYNCIO%-python-agent-junit.xml"
 if "%ASYNCIO%" == "true" (
-    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
 if "%ASYNCIO%" == "false" (
-    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" %IGNORE_PYTHON3_WITH_PYTHON2% --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
+    %VENV_PYTHON%\python.exe -m pytest %PYTEST_JUNIT% --ignore-glob="*\asyncio*\*" --cov --cov-context=test --cov-branch --cov-config=setup.cfg -m "not integrationtest" || exit /b 1
 )
 call %VENV_PYTHON%\python.exe setup.py bdist_wheel
