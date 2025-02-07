af8499d2f7b76b10e1e8eaadeca9207aacb1a929
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-02-07 09:32:04 +0100

    elasticapm: rework buffering of apm data
    With Python 3.13 our pattern of buffering broke because on exit the
    underlying BytesIO fileobj gets released before gzip.GzipFile. So move
    to keep the full buffer uncompressed and compress it before release.

--- elasticapm/transport/base.py
+++ elasticapm/transport/base.py
@@ -153,7 +153,7 @@ class Transport(ThreadManager):
                     buffer_written = True
                     self._counts[event_type] += 1
 
-            queue_size = 0 if buffer.fileobj is None else buffer.fileobj.tell()
+            queue_size = 0 if buffer is None else buffer.tell()
 
             forced_flush = flush
             if forced_flush:
@@ -219,7 +219,7 @@ class Transport(ThreadManager):
         return data
 
     def _init_buffer(self):
-        buffer = gzip.GzipFile(fileobj=io.BytesIO(), mode="w", compresslevel=self._compress_level)
+        buffer = io.BytesIO()
         return buffer
 
     def _write_metadata(self, buffer) -> None:
@@ -251,10 +251,7 @@ class Transport(ThreadManager):
         if not self.state.should_try():
             logger.error("dropping flushed data due to transport failure back-off")
         else:
-            fileobj = buffer.fileobj  # get a reference to the fileobj before closing the gzip file
-            buffer.close()
-
-            data = fileobj.getbuffer()
+            data = gzip.compress(buffer.getvalue(), compresslevel=self._compress_level)
             try:
                 self.send(data, forced_flush=forced_flush)
                 self.handle_transport_success()
