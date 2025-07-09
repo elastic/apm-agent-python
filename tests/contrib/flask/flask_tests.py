eaeb6a47d452af153de377497774197fc4606452
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-07-09 11:48:24 +0200

    Fix tests

--- tests/contrib/flask/flask_tests.py
+++ tests/contrib/flask/flask_tests.py
@@ -441,32 +441,6 @@ def test_rum_tracing_context_processor(flask_apm_client):
         assert callable(context["apm"]["span_id"])
 
 
-@pytest.mark.parametrize("flask_apm_client", [{"logging": True}], indirect=True)
-def test_logging_enabled(flask_apm_client):
-    logger = logging.getLogger()
-    logger.error("test")
-    error = flask_apm_client.client.events[ERROR][0]
-    assert error["log"]["level"] == "error"
-    assert error["log"]["message"] == "test"
-
-
-@pytest.mark.parametrize("flask_apm_client", [{"logging": False}], indirect=True)
-def test_logging_disabled(flask_apm_client):
-    logger = logging.getLogger()
-    logger.error("test")
-    assert len(flask_apm_client.client.events[ERROR]) == 0
-
-
-@pytest.mark.parametrize("flask_apm_client", [{"logging": logging.ERROR}], indirect=True)
-def test_logging_by_level(flask_apm_client):
-    logger = logging.getLogger()
-    logger.warning("test")
-    logger.error("test")
-    assert len(flask_apm_client.client.events[ERROR]) == 1
-    error = flask_apm_client.client.events[ERROR][0]
-    assert error["log"]["level"] == "error"
-
-
 def test_flask_transaction_ignore_urls(flask_apm_client):
     resp = flask_apm_client.app.test_client().get("/users/")
     resp.close()
