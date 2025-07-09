a9336b571b9bd59cba076cd891f6039bdbc7212f
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-07-09 15:44:31 +0200

    Remove usage of deprecated pkg_resource in favor of importlib.metadata
    In setup.py just remove a check for a 2018+ release of setuptools.

--- elasticapm/__init__.py
+++ elasticapm/__init__.py
@@ -28,6 +28,7 @@
 #  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 #  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 import sys
+import importlib.metadata
 
 from elasticapm.base import Client, get_client  # noqa: F401
 from elasticapm.conf import setup_logging  # noqa: F401
@@ -54,10 +55,7 @@ __all__ = ("VERSION", "Client")
 _activation_method = None
 
 try:
-    try:
-        VERSION = __import__("importlib.metadata").metadata.version("elastic-apm")
-    except ImportError:
-        VERSION = __import__("pkg_resources").get_distribution("elastic-apm").version
+    VERSION = importlib.metadata.version("elastic-apm")
 except Exception:
     VERSION = "unknown"
 
