b34752245435e357be4f613f8c36c79666d22550
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-10-29 16:29:16 +0100

    Drop testing against ancient elasticsearch versions

--- tests/scripts/envs/elasticsearch-5.sh
+++
@@ -1,5 +0,0 @@
-export PYTEST_MARKER="-m elasticsearch"
-export ES_URL="http://elasticsearch5:9200"
-export DOCKER_DEPS="elasticsearch5"
-export WAIT_FOR_HOST="elasticsearch5"
-export WAIT_FOR_PORT=9200
